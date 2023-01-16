c684cffeb9e9950f27ae53856cd09bf1bb3b9e40
Eric Haag <eah0592@gmail.com>
2023-01-16 15:43:59 -0600

    Delete public directory on every Hugo build

--- build-logic/hugo/src/main/kotlin/dev/erichaag/hugo/HugoBuild.kt
+++ build-logic/hugo/src/main/kotlin/dev/erichaag/hugo/HugoBuild.kt
@@ -2,6 +2,7 @@ package dev.erichaag.hugo
 
 import org.gradle.api.file.ConfigurableFileTree
 import org.gradle.api.file.DirectoryProperty
+import org.gradle.api.file.FileSystemOperations
 import org.gradle.api.provider.Property
 import org.gradle.api.tasks.CacheableTask
 import org.gradle.api.tasks.IgnoreEmptyDirectories
@@ -11,9 +12,12 @@ import org.gradle.api.tasks.OutputDirectory
 import org.gradle.api.tasks.PathSensitive
 import org.gradle.api.tasks.PathSensitivity
 import org.gradle.api.tasks.TaskAction
+import javax.inject.Inject
 
 @CacheableTask
-abstract class HugoBuild : AbstractHugoExecTask() {
+abstract class HugoBuild @Inject constructor(
+  private val fileSystemOperations: FileSystemOperations,
+) : AbstractHugoExecTask() {
 
   @get:InputFiles
   @get:IgnoreEmptyDirectories
@@ -30,12 +34,17 @@ abstract class HugoBuild : AbstractHugoExecTask() {
   abstract val publicDirectory: DirectoryProperty
 
   @TaskAction
-  fun action() = hugoExec {
-    args("--source", sourceFiles.dir.path)
-    args("--destination", publicDirectory.get().asFile.path)
-    args("--noBuildLock")
-    args("--minify")
-    args("--theme", themeName.get())
-    if (buildDrafts.get()) args("--buildDrafts")
+  fun action() {
+    fileSystemOperations.delete {
+      delete(publicDirectory)
+    }
+    hugoExec {
+      args("--source", sourceFiles.dir.path)
+      args("--destination", publicDirectory.get().asFile.path)
+      args("--noBuildLock")
+      args("--minify")
+      args("--theme", themeName.get())
+      if (buildDrafts.get()) args("--buildDrafts")
+    }
   }
 }
