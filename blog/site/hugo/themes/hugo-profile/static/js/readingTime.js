44533eb2e578b81886ffccd3f89893c982c239c7
Eric Haag <eah0592@gmail.com>
2024-04-07 00:03:55 -0500

    Add space to reading time

--- blog/site/hugo/themes/hugo-profile/static/js/readingTime.js
+++ blog/site/hugo/themes/hugo-profile/static/js/readingTime.js
@@ -5,6 +5,6 @@ function readingTime() {
   const time = Math.ceil(words / wpm);
 
   const timeElement = document.querySelector("span#readingTime");
-  timeElement.innerHTML = "<small> | </small>" + time + timeElement.innerHTML;
+  timeElement.innerHTML = "<small> | </small>" + time + " " + timeElement.innerHTML;
 }
 readingTime();
